748566099e5e17f6df4d56b735296eb31a33a5bf
Kenny Walker <Kenny>
2025-11-15 21:58:26 +0700

    RESPONSIVE Index + Added pop-up notif & header js for all html

--- js/contact.js
+++ js/contact.js
@@ -1,21 +1,3 @@
-// Popup thông báo Tìm kiếm
-document.addEventListener("DOMContentLoaded", () => {
-  const searchIcon = document.querySelector(".search-icon");
-  const popup = document.getElementById("search-popup");
-
-  searchIcon.addEventListener("click", (e) => {
-    e.preventDefault();
-    popup.classList.remove("hidden");
-    popup.classList.add("show");
-
-    setTimeout(() => {
-      popup.classList.remove("show");
-      popup.classList.add("hidden");
-    }, 5000);
-  });
-});
-
-
 // Popup thông báo Đã submit form 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".contact-form"); 
